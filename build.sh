14a228fa91913cdac2e8e4bcf317b6d232d59e0a
xyen8180 <88905324+xyen8180@users.noreply.github.com>
2021-09-14 13:52:31 +0530

    Update build.sh

--- build.sh
+++ build.sh
@@ -2,10 +2,12 @@
 
 # Just a basic script U can improvise lateron asper ur need xD 
 
-MANIFEST="https://github.com/SHRP/platform_manifest_twrp_omni -b v3_10.0"
-DEVICE=E6746
-DT_LINK="https://github.com/mastersenpai05/twrp_micromax_e6746 -b shrp-10.0"
-DT_PATH=device/micromax/$DEVICE
+MANIFEST="https://github.com/minimal-manifest-twrp/platform_manifest_twrp_aosp -b twrp-11"
+
+
+DEVICE=star
+DT_LINK="https://github.com/xyen8180/test-tree -b Android-11.0"
+DT_PATH=device/xiaomi/$DEVICE
 
 echo " ===+++ Setting up Build Environment +++==="
 apt install openssh-server -y
@@ -23,14 +25,14 @@ git clone --depth=1 $KT_LINK $KT_PATH
 echo " ===+++ Building Recovery +++==="
 . build/envsetup.sh
 export ALLOW_MISSING_DEPENDENCIES=true
-lunch omni_${DEVICE}-eng && mka recoveryimage
+lunch twrp_${DEVICE}-eng && mka bootimage
 
-# Upload zips & recovery.img (U can improvise lateron adding telegram support etc etc)
+# Upload zips & boot.img (U can improvise lateron adding telegram support etc etc)
 echo " ===+++ Uploading Recovery +++==="
 #version=$(cat bootable/recovery/variables.h | grep "define TW_MAIN_VERSION_STR" | cut -d \" -f2)
 #OUTFILE=TWRP-${version}-${DEVICE}-$(date "+%Y%m%d-%I%M").zip
 cd out/target/product/$DEVICE
-#mv recovery.img ${OUTFILE%.zip}.img
+#mv boot.img ${OUTFILE%.zip}.img
 #zip -r9 $OUTFILE ${OUTFILE%.zip}.img
 
 #curl -T $OUTFILE https://oshi.at
